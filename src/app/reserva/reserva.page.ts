5ac8e7393ac04f327b7cc8b7d3d72698e4fe3172
julcontreraso <127865723+julcontreraso@users.noreply.github.com>
2023-11-18 18:08:13 -0300

    Update reserva.page.ts

--- src/app/reserva/reserva.page.ts
+++ src/app/reserva/reserva.page.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservaPage implements OnInit {
 
+  selectedDate: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  isWeekday = (dateString: string): boolean => {
+    if (!dateString) {
+      // Puedes manejar el caso en el que no se haya seleccionado ninguna fecha
+      console.log('Selecciona una fecha');
+      return false;
+    }
+
+    const date = new Date(dateString);
+    const utcDay = date.getUTCDay();
+
+    /**
+     * Date será habilitado si no es
+     * domingo (0) o sábado (6)
+     */
+    return utcDay !== 0 && utcDay !== 6;
+  };
+
+  // Función para confirmar la reserva
+  confirmarReserva() {
+    if (!this.selectedDate) {
+      // Puedes manejar el caso en el que no se haya seleccionado ninguna fecha
+      console.log('Selecciona una fecha');
+      return;
+    }
+
+    if (this.isWeekday(this.selectedDate)) {
+      // Aquí puedes agregar la lógica para confirmar la reserva en un día laborable
+      console.log('Reserva confirmada para la fecha:', this.selectedDate);
+    } else {
+      // Muestra un mensaje de error o realiza alguna acción si la fecha no es un día laborable
+      console.log('Selecciona un día de lunes a viernes');
+      // También puedes reiniciar la fecha seleccionada a un valor predeterminado
+      // this.selectedDate = valorPorDefecto;
+    }
+  }
 }
